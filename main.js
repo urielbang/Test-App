0debe73a19cccb9c209f8f8d3a1e0f5f146eac5f
urielbang <uri32655@gmail.com>
2024-08-27 09:52:54 +0300

    add search by text

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const apiKey = "42079125-1909400cd5615db78c9c2cb93";
 const url = "https://pixabay.com/api/?key=";
 const mainElement = document.querySelector(".mainPictures");
 const input = document.getElementById("seacrhInput");
+const buttonSearch = document.querySelector(".seacrhButton");
+const formInput = document.getElementById("container-form");
 
 async function fetchData() {
   try {
@@ -25,6 +27,28 @@ async function fetchData() {
 
 fetchData();
 
-input.addEventListener("change", (e) => {
-  console.log(e.target.value);
-});
+async function onClickSearch(e) {
+  e.preventDefault();
+  console.dir(e.srcElement[0].value);
+
+  try {
+    const resFetch = await fetch(`${url}${apiKey}`);
+    const data = await resFetch.json();
+    const filteredData = data.hits.filter((picture) => {
+      return picture.tags.includes(e.srcElement[0].value);
+    });
+
+    const dataElements = filteredData.map((pictureData) => {
+      return `<div class="card-picture">
+          <img src=${pictureData.previewURL} alt="picture">
+          <p>${pictureData.tags}</p>
+      </div>`;
+    });
+
+    mainElement.innerHTML = dataElements;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+formInput.addEventListener("submit", onClickSearch);
